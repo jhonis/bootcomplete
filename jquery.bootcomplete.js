46a5bae82bcd4ac65a9b0fd97b04d0e7a7db464d
Jhonis de Souza <jhonis.souza@gmail.com>
2014-07-31 18:07:13 -0300

    New functionalities and bootstrap 2 compatibility.

--- jquery.bootcomplete.js
+++ jquery.bootcomplete.js
@@ -12,6 +12,19 @@
 
         var settings = $.extend({}, $.fn.bootcomplete.defaults, options);
 
+        var bootstrapClasses = {};
+        if (settings.bootstrapVersion == 3) {
+            bootstrapClasses.inputGroup = 'input-group';
+            bootstrapClasses.formControl = 'form-control';
+            bootstrapClasses.btn = 'btn btn-default';
+        } else if (settings.bootstrapVersion == 2) {
+            bootstrapClasses.inputGroup = 'input-append span10';
+            bootstrapClasses.formControl = 'input-block-level';
+            bootstrapClasses.btn = 'btn';
+        } else {
+            alert('Bootstrap version incompatible');
+        }
+
         function clear() {
             $(instance).val('');
             $('#bc-hidden-'+ id).val('');
@@ -20,65 +33,89 @@
         }
 
         function print() {
-            $(instance).blur(function() {
-                $(this).val('');
-            });
-            $(instance).autocomplete({
-                minLength: settings.minLength,
-                delay: settings.delay,
-                select: function (event, ui) {
-                    $('#bc-hidden-'+ id).val(ui.item.id);
-                    $('#bc-selected-'+ id).val(ui.item.value);
-                    $(instance).val(ui.item.value);
-                    $(instance).hide();
-                    $('#bc-div-selected-'+ id).show();
-                },
-                close: function() {
-                    $(instance).val('');
-                },
-                source: function (request, response) {
-                    var dataObj = {};
-                    dataObj[settings.parameterName] = $(instance).val();
-                    $.ajax({
-                        url: settings.url,
-                        data: dataObj,
-                        success: function (data) {
-                            response($.map(data, function (item) {
-                                return {
-                                    label: item[settings.labelAttributeName],
-                                    id: item[settings.idAttributeName]
-                                }
-                            }));
+            if (!$(instance).is(":disabled")) {
+                $(instance).blur(function () {
+                    $(this).val('');
+                });
+                $(instance).autocomplete({
+                    minLength: settings.minLength,
+                    delay: settings.delay,
+                    select: function (event, ui) {
+                        $('#bc-hidden-' + id).val(ui.item.id);
+                        $('#bc-selected-' + id).val(ui.item.value);
+                        $(instance).val(ui.item.value);
+                        $(instance).hide();
+                        $('#bc-div-selected-' + id).show();
+                    },
+                    close: function () {
+                        $(instance).val('');
+                    },
+                    source: function (request, response) {
+                        var dataObj = {};
+                        dataObj[settings.parameterName] = $(instance).val();
+                        if (typeof settings.additionalSelector != 'undefined' && settings.additionalSelector != '' &&
+                            typeof settings.additionalParameterName != 'undefined' && settings.additionalParameterName != '') {
+                            dataObj[settings.additionalParameterName] = $(settings.additionalSelector).val();
                         }
-                    });
+                        $.ajax({
+                            url: settings.url,
+                            data: dataObj,
+                            success: function (data) {
+                                response($.map(data, function (item) {
+                                    var itemReturn = {};
+                                    if ($.isFunction(settings.labelAttribute)) {
+                                        itemReturn.label = settings.labelAttribute(item);
+                                    } else {
+                                        itemReturn.label = item[settings.labelAttribute];
+                                    }
+                                    if ($.isFunction(settings.idAttribute)) {
+                                        itemReturn.id = settings.idAttribute(item);
+                                    } else {
+                                        itemReturn.id = item[settings.idAttribute];
+                                    }
+                                    return itemReturn;
+                                }));
+                            }
+                        });
+                    }
+                });
+
+                var hidden = $('<input type="hidden"/>');
+                hidden.attr('id', 'bc-hidden-' + id);
+                hidden.attr('name', name);
+                hidden.val(value);
+
+                var divSelected = $('<div/>');
+                divSelected.attr('id', 'bc-div-selected-' + id);
+                divSelected.addClass(bootstrapClasses.inputGroup);
+                if (settings.bootstrapVersion == 2) {
+                    divSelected.css({'margin-left': '0px'});
                 }
-            });
+                if (typeof value == 'undefined' || value == '') {
+                    divSelected.css({display: 'none'});
+                } else {
+                    $(instance).hide();
+                }
+                var inputGroup;
+                if (settings.bootstrapVersion == 3) {
+                    inputGroup = $('<span/>').addClass('input-group-btn');
+                }
+                var inputSelected = $('<input type="text"/>').attr('id', 'bc-selected-' + id).addClass(bootstrapClasses.formControl).val(text).attr('disabled', 'disabled');
+                var removeButton = $('<button type="button"/>').addClass(bootstrapClasses.btn).click(clear);
+                var glyph = $('<span/>').addClass('glyphicon glyphicon-remove');
+                removeButton.append(glyph);
+                removeButton.append(' Remover');
+                divSelected.append(inputSelected);
+                if (settings.bootstrapVersion == 3) {
+                    inputGroup.append(removeButton);
+                    divSelected.append(inputGroup);
+                } else {
+                    divSelected.append(removeButton);
+                }
 
-            var hidden = $('<input type="hidden"/>');
-            hidden.attr('id', 'bc-hidden-'+ id);
-            hidden.attr('name', name);
-            hidden.val(value);
-
-            var divSelected = $('<div/>');
-            divSelected.attr('id', 'bc-div-selected-'+ id);
-            divSelected.addClass('input-group');
-            if (typeof value == 'undefined' || value == '') {
-                divSelected.css({display: 'none'});
-            } else {
-                $(instance).hide();
+                $(instance).parent().append(hidden);
+                $(instance).parent().append(divSelected);
             }
-            var inputGroup = $('<span/>').addClass('input-group-btn');
-            var inputSelected = $('<input type="text"/>').attr('id', 'bc-selected-'+ id).addClass('form-control').val(text).attr('disabled', 'disabled');
-            var removeButton = $('<button type="button"/>').addClass('btn btn-default').click(clear);
-            var glyph = $('<span/>').addClass('glyphicon glyphicon-remove');
-            removeButton.append(glyph);
-            removeButton.append(' Remover');
-            inputGroup.append(removeButton);
-            divSelected.append(inputSelected);
-            divSelected.append(inputGroup);
-
-            $(instance).parent().append(hidden);
-            $(instance).parent().append(divSelected);
         }
 
         return print();
@@ -94,10 +131,13 @@
     $.fn.bootcomplete.defaults = {
         minLength: 2,
         delay: 200,
+        bootstrapVersion: 3,
         url: '',
-        labelAttributeName: 'text',
-        idAttributeName: 'id',
-        parameterName: 'value'
+        labelAttribute: 'name',
+        idAttribute: 'id',
+        parameterName: 'value',
+        additionalSelector: '',
+        additionalParameterName: ''
     };
 
 }(jQuery));
